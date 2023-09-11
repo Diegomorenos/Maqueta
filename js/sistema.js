e9057ec2c8a85567a749da3f973520d1212c9b72
Diegomorenos <d1360ms@gmail.com>
2023-09-10 21:22:19 -0500

    formularios

--- js/sistema.js
+++ js/sistema.js
@@ -10,5 +10,32 @@ function seleccionar(link){
     links[5].className="";
 
     link = className="selected";
+}
 
+function contactar() {
+    let name = document.getElementById("name");
+    let lastname = document.getElementById("lastname");
+    let email = document.getElementById("email");
+    let text = document.getElementById("text");
+    let valemail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/ ;
+
+    if (name.value=="") {
+        alert("El nombre no es valido");
+        exit();
+    }
+    if (lastname.value=="") {
+        alert("El apellido no es valido");
+        exit();
+    }
+    if (!valemail.test(email.value)) {
+        alert("El email no es valido");
+        exit();
+    }
+    if (text.value.length < 8) {
+        alert("Olvidaste escribir un mensaje para nosotros!");
+        exit();
+    }
+    else{
+        alert("Mensaje enviado")
+    }
 }
